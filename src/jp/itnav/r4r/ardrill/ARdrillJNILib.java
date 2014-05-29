4847090ad000e10ec9770cc1b06e4d4ce8e4ad13
Kenichi Takahashi <ktaka@smarteducation.jp>
2014-05-30 08:53:51 +0900

    Added setting function for animation speed and water level.

--- src/jp/itnav/r4r/ardrill/ARdrillJNILib.java
+++ src/jp/itnav/r4r/ardrill/ARdrillJNILib.java
@@ -31,7 +31,6 @@ public class ARdrillJNILib {
      * @param height the current view height
      */
      public static native void setUp(String extPath, AssetManager assetManager, int avatarType);
-     public static native void setUp(String extPath, AssetManager assetManager);
      public static native void init(int width, int height);
      public static native void step();
      public static native void gainedFocus();
@@ -39,4 +38,6 @@ public class ARdrillJNILib {
      public static native void pause();
      public static native void setCameraAxis(float x, float y, float z, float w);
      public static native void setAnimationNumber(int number);
+     public static native void setWaterLevel(float level);
+     public static native void setAnimationSpeed(float speed);
 }
